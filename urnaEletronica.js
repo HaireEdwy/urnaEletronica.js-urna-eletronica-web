3b794e29838ddedd0009d2aabae2c7d22f6cfddf
haireedwy <haire.edwy@hotmail.com>
2023-06-02 10:29:50 -0300

    inicio do desafio

--- urnaEletronica.js
+++ urnaEletronica.js
@@ -1,1 +1,15 @@
+function audioConfirmacao(){
+    const audio = new Audio("./confirmacao.mp3");
+    audio.play();
+}
+
+// if (codigoVoto ! == null){
+//    codigoVoto = parseInt(codigoVoto);
+// } else {
+//     break;
+// }
+
+/*OS BOTÕES VÃO NO IDES.HTML
+<button type="button" onclick="urnaEletronica()">Começar</button>
+<button type="button" onclick="audioConfirmacao()">"Testar audio"</button>*/
 
